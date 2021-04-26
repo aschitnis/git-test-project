2d7c336070dedd19c803dc71ed139a5b5dee1e05
Abi Chitnis <aschitnis@hotmail.com>
2021-04-26 17:28:15 +0200

    added model classes 26.04.2021 17:28

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,16 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo (World) Git Repository !!!");
-            Console.WriteLine("Issue-054 is being resolved....");
-			Console.WriteLine("Hotfix is resolved");
-			Console.WriteLine("Issue-056 is resolving currently....");
-			Console.WriteLine("Issue-056 is resolved....");
-			Console.WriteLine("Hotfix-02 is resolved");
-			Console.WriteLine("Issue-060 is resolving currently....");
-			Console.WriteLine("Issue-061 is resolved @22042021 16:08");
-			Console.WriteLine("Issue-061 is resolving currently @22042021 17:08");
-			Console.WriteLine("Gmail-Collaborator has added this Line @23042021 10:56 AM")
-			Console.WriteLine("Gmail-Collaborator has added Currency changes @23042021 11:15 AM")
+            Console.WriteLine("added class models/impl. data-Containers  26.04.21 17:26 PM");
             Console.ReadLine();
         }
     }
